a0e80f462940f033c99bccb6582caab95e7d52f8
Student <student@dailynote.dev>
2020-12-23 23:48:05 -0500

    Wed Dec 23 23:48:05 EST 2020

---
+++ 785_disconnected_graph.py
@@ -0,0 +1,17 @@
+from typing import List
+
+def split_graph(graph: List[List[int]]) -> List[List[List[int]]] :
+    ''' Getting a unidirectional graph as an argument, return List of connected graphs '''
+    visited = [False]*len(graph)
+    result = []
+
+    while not all(visited):
+        # Performing BFS starting from first not visited
+        node = visited.index(False)
+
+        current_graph = []
+        current_visited = {}
+
+        
+
+
