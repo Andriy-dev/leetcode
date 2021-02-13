26b006303d489533daa4d1e41ab422bc3e78e678
Student <student@dailynote.dev>
2021-02-13 18:40:27 -0500

    261

--- 785_disconnected_graph.py
+++ 785_disconnected_graph.py
@@ -1,17 +1,27 @@
 from typing import List
 
+def traversal(graph: List[List[int]]),source):
+
+
 def split_graph(graph: List[List[int]]) -> List[List[List[int]]] :
     ''' Getting a unidirectional graph as an argument, return List of connected graphs '''
     visited = [False]*len(graph)
     result = []
 
     while not all(visited):
-        # Performing BFS starting from first not visited
+        # Performing inorder traversal starting from first not visited
         node = visited.index(False)
 
-        current_graph = []
-        current_visited = {}
+        # [ [List] [List] [List] ]
+        
+        stack = [node]
+        localgraph = []
 
+        while stack:
+            node = stack.pop()
+            if node not in graph:
+                localgraph.append(node)
+            for leaf in graph[node]
         
 
 
